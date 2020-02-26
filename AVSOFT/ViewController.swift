e1ffa13a545ad0ab97875bc9b7db6838992cea97
DaniilGirskiy <https://github.com/DaniilGirskiy>
2020-02-26 12:14:12 +0300

    Initial Commit

---
+++ AVSOFT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVSOFT
+//
+//  Created by Danya on 26.02.2020.
+//  Copyright © 2020 Daniil Girskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
